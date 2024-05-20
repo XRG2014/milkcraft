2a3d0a6d17eac2c078a1714501b0cd7bcf8a4585
XRG2014 <138714694+XRG2014@users.noreply.github.com>
2024-05-20 16:32:41 -0700

    Update serverstatus.js

--- docs/assets/serverstatus.js
+++ docs/assets/serverstatus.js
@@ -7,8 +7,8 @@
     document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Play Now: ${data.players.online} Online</span>`;
     {% else %}
     var delstat = document.getElementById("serverStatus")
-    delstat.parentNode.removeChild(delstat);
+    delstat.remove()
     var delstatm = document.getElementById("serverStatusM")
-    delstatm.parentNode.removeChild(delstatm);
+    delstatm.remove()
     {% endif %}
 });
