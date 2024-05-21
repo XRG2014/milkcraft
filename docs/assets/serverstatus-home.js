1c660e3cfe50bde5104370548d84407a5bc5720b
XRG2014 <138714694+XRG2014@users.noreply.github.com>
2024-05-20 17:08:41 -0700

    Update serverstatus-home.js

--- docs/assets/serverstatus-home.js
+++ docs/assets/serverstatus-home.js
@@ -7,6 +7,4 @@
     document.getElementById("serverInd").classList.add("text-green-600");
     document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Play Now: ${data.players.online} Online</span>`;
     {% endif %}
-    document.getElementById("serverStatus").remove()
-    document.getElementById("serverStatusM").remove()
 });
